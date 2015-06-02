e0f0c683d4a5add1804c241c05932116c9374166
dong <dongorigin@gmail.com>
2015-06-02 17:32:35 +0800

    保存图片到外部存储，并通知图库更新

--- demo/src/main/java/cn/dong/demo/ui/storage/BitmapSaveLocalActivity.java
+++ demo/src/main/java/cn/dong/demo/ui/storage/BitmapSaveLocalActivity.java
@@ -1,10 +1,11 @@
 package cn.dong.demo.ui.storage;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -13,63 +14,86 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 import cn.dong.demo.R;
 import cn.dong.demo.ui.common.BaseActivity;
+import cn.dong.demo.util.L;
 
 /**
  * 将应用内资源图片存储至储存卡
- * 
+ *
  * @author dong 2014-5-23
  */
 public class BitmapSaveLocalActivity extends BaseActivity {
-	private static final String IMAGE_NAME = "ic_launcher.png";
+    private static final String IMAGE_NAME = "ic_launcher.png";
 
-	private Button btn_save;
+    private Button btn_save;
 
-	@Override
-	protected int initPageLayoutID() {
-		return R.layout.activity_image;
-	}
+    @Override
+    protected int initPageLayoutID() {
+        return R.layout.activity_image;
+    }
 
-	@Override
-	protected void initPageView() {
-		btn_save = (Button) findViewById(R.id.save);
-	}
+    @Override
+    protected void initPageView() {
+        btn_save = (Button) findViewById(R.id.save);
+    }
 
-	@Override
-	protected void initPageViewListener() {
-		btn_save.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				a();
-			}
-		});
-	}
+    @Override
+    protected void initPageViewListener() {
+        btn_save.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveImage();
+            }
+        });
+    }
 
-	@Override
-	protected void process(Bundle savedInstanceState) {
+    @Override
+    protected void process(Bundle savedInstanceState) {
 
-	}
+    }
 
-	private void a() {
-		Log.d(TAG, getExternalFilesDir("").toString());
-		Log.d(TAG, getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString());
+    private void saveImage() {
+//        L.d(TAG, getExternalFilesDir("").toString());
+//        L.d(TAG, getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString());
+//        L.d(TAG, Environment.getExternalStorageDirectory().toString());
+//        L.d(TAG, Environment.getExternalStoragePublicDirectory("test").toString());
 
-		Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
-		File extStorageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-		File file = new File(extStorageDirectory, IMAGE_NAME);
-		try {
-			OutputStream os = new FileOutputStream(file);
-			bitmap.compress(Bitmap.CompressFormat.PNG, 100, os);
-			os.flush();
-			os.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        // 保存图片
+        File appDir = new File(Environment.getExternalStorageDirectory(), "DongDemo");
+        if (!appDir.exists()) {
+            if (!appDir.mkdir()) {
+                return;
+            }
+        }
+        String fileName = System.currentTimeMillis() + ".jpg";
+        File file = new File(appDir, fileName);
+        L.d(TAG, "Path = " + file.getAbsolutePath());
+        L.d(TAG, "Parent = " + file.getParent());
+
+        try {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+            FileOutputStream fos = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 70, fos);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 将文件插入系统图库
+        // 一直提示失败
+//        try {
+//            MediaStore.Images.Media.insertImage(mContext.getContentResolver(), file.getAbsolutePath(), fileName, null);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
+        // 通知系统图库更新
+        mContext.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(file)));
+    }
 
 }
